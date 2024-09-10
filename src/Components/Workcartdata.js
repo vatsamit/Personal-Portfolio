77a5969590bef8d2e6c0f02ab41e7daf45aff166
vatsamit <128714694+vatsamit@users.noreply.github.com>
2024-09-10 15:10:29 +0530

    Update Workcartdata.js
    linlk added(latest)

--- src/Components/Workcartdata.js
+++ src/Components/Workcartdata.js
@@ -35,7 +35,7 @@ const ProjectCardData = [
     title : "Personal Portfolio",
     text : "Explore my React-powered portfolio, showcasing diverse projects with seamless navigation and dynamic user experiences. Leveraging React components, state management, and props, the site adapts to various devices and user interactions. With React Router, easily delve into project details and personal information.Discover my passion for technology and innovation in this comprehensive portfolio, encapsulating my journey, skills, and aspirations" ,
     
-    view : "https://github.com/vatsamit/Personal-Portfolio",
+    view : "https://personal-portfolio-infinite.vercel.app/",
     source :"https://github.com/vatsamit/Personal-Portfolio.git "
 } ,
 
